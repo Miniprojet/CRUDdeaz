a2e26da92b66a8151ac8bd354ce29a216e74e15a
Miniprojet <elb.muhamed@gmail.com>
2014-10-04 16:44:06 +0000

    Finished Classes
    next step configuration XML files

---
+++ src/com/medbarcha/crud/service/impl/StudentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.medbarcha.crud.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.medbarcha.crud.dao.StudentDao;
+import com.medbarcha.crud.dao.impl.StudentDaoImpl;
+import com.medbarcha.crud.model.Student;
+import com.medbarcha.crud.service.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+
+	@Autowired
+	private StudentDao studentDao;
+	
+	@Transactional
+	public void add(Student student) {
+		studentDao.add(student);
+	}
+
+	@Transactional
+	public void edit(Student student) {
+		studentDao.edit(student);
+	}
+
+	@Transactional
+	public void delete(int studentId) {
+		studentDao.delete(studentId);
+	}
+
+	@Transactional
+	public Student getStudent(int studentId) {
+		return studentDao.getStudent(studentId);
+	}
+
+	@Transactional
+	public List getAllStudent() {
+		return studentDao.getAllStudent();
+	}
+
+}
